31ee9cc721b78487aa1452ab1b242e269c65ee24
qinmingyuan <mingyuan0715@foxmail.com>
2020-07-30 19:10:41 +0800

    got members

--- app/models/rails_bench/tasking.rb
+++ app/models/rails_bench/tasking.rb
@@ -6,9 +6,9 @@ module RailsBench::Tasking
 
     has_many :tasks, as: :tasking, dependent: :destroy
 
-    has_many :task_masters, foreign_key: :tasking_type, primary_key: :record_name
-    has_many :members, through: :task_masters
     has_many :task_templates, -> { roots }, foreign_key: :tasking_type, primary_key: :record_name
+    has_many :job_titles, through: :task_templates
+    has_many :members, through: :task_templates
   end
 
   def to_task(member_id, task_template_id)
