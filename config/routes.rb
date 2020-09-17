df0be2d0e29f72b3c876b907cccfd50318a1ae06
qinmingyuan <mingyuan0715@foxmail.com>
2020-09-17 11:11:53 +0800

    use common project bar

--- config/routes.rb
+++ config/routes.rb
@@ -53,6 +53,7 @@ Rails.application.routes.draw do
 
   scope :admin, module: 'bench/admin', as: :admin do
     resources :project_taxons
+    resources :project_
     resources :task_templates do
       collection do
         get :add
