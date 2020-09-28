f52e857736e3d1b95f0bc54af62e6bbb664c1b51
qinmingyuan <mingyuan0715@foxmail.com>
2020-09-28 18:44:25 +0800

    make that simple

--- app/models/rails_bench/member.rb
+++ app/models/rails_bench/member.rb
@@ -4,11 +4,8 @@ module RailsBench::Member
   included do
     attribute :pomodoro, :integer
 
-    belongs_to :provider, optional: true
-
     has_many :tasks, dependent: :nullify
-    has_many :project_members, dependent: :nullify
-    has_many :projects, through: :project_members
+    has_many :projects, foreign_key: :creator_id
     has_many :task_templates, dependent: :destroy
   end
 
