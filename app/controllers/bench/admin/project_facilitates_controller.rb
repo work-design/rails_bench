b48fd4f8685f2e62585a81b5d07c8ffeaa2a9174
lewislinlin <lewislinlin@gmail.com>
2020-10-09 11:12:49 +0800

    form add facilitate_taxons select

--- app/controllers/bench/admin/project_facilitates_controller.rb
+++ app/controllers/bench/admin/project_facilitates_controller.rb
@@ -39,6 +39,12 @@ class Bench::Admin::ProjectFacilitatesController < Bench::Admin::BaseController
   end
 
   # slect options
+  def facilitates
+    q_params = {}
+    q_params.merge! facilitate_taxon_id: project_facilitate_params[:facilitate_taxon_id]
+    @facilitates = Facilitate.default_where(q_params)
+  end
+
   def providers
     @facilitate = Facilitate.find project_facilitate_params[:facilitate_id]
     @providers = @facilitate.providers
@@ -55,13 +61,15 @@ class Bench::Admin::ProjectFacilitatesController < Bench::Admin::BaseController
 
   def project_facilitate_params
     params.fetch(:project_facilitate, {}).permit(
+      :facilitate_taxon_id,
       :facilitate_id,
       :provider_id
     )
   end
 
   def prepare_form
-    @facilitates = Facilitate.all
+    @facilitate_taxons = FacilitateTaxon.limit(500)
+    @facilitates = Facilitate.limit(500)
     @providers = @project_facilitate&.facilitate&.providers || []
   end
 
