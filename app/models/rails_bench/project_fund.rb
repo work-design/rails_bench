6ea01a61e681cef1b3b4d3cde824cd1aefb16c6a
lewislinlin <lewislinlin@gmail.com>
2020-10-10 18:00:53 +0800

    add fund_budget

--- app/models/rails_bench/project_fund.rb
+++ app/models/rails_bench/project_fund.rb
@@ -2,6 +2,7 @@ module RailsBench::ProjectFund
   extend ActiveSupport::Concern
 
   included do
+    attribute :budget, :decimal
     attribute :amount, :decimal
     attribute :visible, :boolean, default: true
     attribute :note, :string
@@ -11,11 +12,11 @@ module RailsBench::ProjectFund
 
     has_one_attached :proof
 
-    after_save :sum_amount, if: -> { saved_change_to_amount? }
+    after_save :sum_amount, if: -> { saved_change_to_amount? || saved_change_to_budget? }
   end
 
   def sum_amount
-    project.compute_fund_amount
+    project.compute_fund_budget_amount
     project.save
   end
 
