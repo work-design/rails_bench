1325ecb2e4aba31bb94081924dce9702f5cb092a
qinmingyuan <mingyuan0715@foxmail.com>
2020-10-17 15:08:09 +0800

    move resource to admin

--- app/controllers/bench/admin/tasks_controller.rb
+++ app/controllers/bench/admin/tasks_controller.rb
@@ -1,11 +1,150 @@
 class Bench::Admin::TasksController < Bench::Admin::BaseController
 
+  before_action :set_task, only: [
+    :show, :edit, :update, :edit_focus, :edit_assign, :reorder, :edit_done, :update_done, :rework, :destroy
+  ]
+  before_action :prepare_form, only: [:show]
+
+  def index
+    q_params = {
+      focus: ['today', 'inbox'],
+      state: ['todo', 'doing'],
+      user_id: current_user.id
+    }
+    q_params.merge! member_id: current_member.id
+    q_params.merge! params.permit(:focus, :state, :tasking_type, :tasking_id)
+    @tasks = Task.includes(:task_timers).roots.default_where(q_params).page(params[:page])
+  end
+
   def project
     q_params = {}
     q_params.merge! params.permit(:state)
 
     @project = Project.find params[:project_id]
     @tasks = @project.tasks.roots.default_where(q_params)
+  end
+
+  def new
+    @task = Task.new raw_task_params
+  end
+
+  def new_template
+    @task = Task.new raw_task_params
+  end
+
+  def create
+    @task = current_member.tasks.build task_params
+
+    if @task.save
+      render 'create'
+    else
+      render :new, locals: { model: @task }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    q_params = {
+      user_id: current_user.id
+    }
+    q_params.merge! params.permit(:state, :focus)
+
+    @tasks = @task.self_and_siblings.includes(:task_timer, :task_timers).default_where(q_params).page(params[:page])
+  end
+
+  def reorder
+    sort_array = params[:sort_array].select { |i| i.integer? }
+
+    if params[:new_index] > params[:old_index]
+      prev_one = @task.same_scopes.find(sort_array[params[:new_index].to_i - 1])
+      @task.insert_at prev_one.position
+    else
+      next_ones = @task.same_scopes.find(sort_array[(params[:new_index].to_i + 1)..params[:old_index].to_i])
+      next_ones.each do |next_one|
+        next_one.insert_at @task.position
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def edit_assign
+    q_params = {
+      'member_departments.job_title_id': @task.job_title_id
+    }
+    q_params.merge! default_params
+    @members = Member.default_where(q_params)
+  end
+
+  def update
+    @task.assign_attributes(task_params)
+
+    unless @task.save
+      render :edit, locals: { model: @task }, status: :unprocessable_entity
+    end
+  end
+
+  def remove
+    render 'update'
+  end
+
+  def edit_focus
+    @task
+  end
+
+  def edit_done
+  end
+
+  def update_done
+    @task.assign_attributes(task_params)
+    @task.done_at = Time.current
+
+    unless @task.save
+      render :edit_done, locals: { model: @task }, status: :unprocessable_entity
+    end
+  end
+
+  def rework
+    @task.set_rework
+  end
+
+  def destroy
+    @task.destroy
+  end
+
+  private
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def prepare_form
+    @task_templates = TaskTemplate.where(tasking_type: @task)
+  end
+
+  def raw_task_params
+    params.permit(
+      :parent_id,
+      :tasking_type,
+      :tasking_id
+    )
+  end
+
+  def task_params
+    p = params.fetch(:task, {}).permit(
+      :tasking_type,
+      :tasking_id,
+      :title,
+      :focus,
+      :content,
+      :parent_id,
+      :member_id,
+      :task_template_id,
+      :estimated_time,
+      :note,
+      :done_at,
+      :proof
+    )
+    p.merge! default_form_params
   end
 
 end
