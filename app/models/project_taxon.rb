d6cc1cbaa0c8a193796f1c07275ac687b51694d3
qinmingyuan <mingyuan0715@foxmail.com>
2020-10-20 16:12:10 +0800

    sep parameter and extra

--- app/models/project_taxon.rb
+++ app/models/project_taxon.rb
@@ -1,6 +1,6 @@
 class ProjectTaxon < ApplicationRecord
   include RailsBench::ProjectTaxon
   include RailsBenchExt::OtherTasking
-  include RailsComExt::Extra
+  include RailsComExt::Parameter
   include RailsFinanceExt::Financial if defined? RailsFinance
 end unless defined? ProjectTaxon
