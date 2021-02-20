8f9439d1341778eaa4edc04b2241308369840aab
qinmingyuan <mingyuan0715@foxmail.com>
2021-02-20 19:51:26 +0800

    index

--- app/models/bench/project_taxon.rb
+++ app/models/bench/project_taxon.rb
@@ -2,6 +2,6 @@ module Bench
   class ProjectTaxon < ApplicationRecord
     include Model::ProjectTaxon
     include Com::Ext::Parameter
-    include RailsFinanceExt::Financial if defined? RailsFinanceExt
+    include Finance::Ext::Financial if defined? RailsFinance
   end
 end
