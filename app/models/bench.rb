d7724c1597fc678ae3d99d63c613a186d9f3c306
qinmingyuan <mingyuan0715@foxmail.com>
2021-05-24 23:47:07 +0800

    bench namespace

---
+++ app/models/bench.rb
@@ -0,0 +1,11 @@
+module Bench
+
+  def self.use_relative_model_naming?
+    true
+  end
+
+  def self.table_name_prefix
+    'bench_'
+  end
+
+end
--- lib/rails_bench.rb
+++ lib/rails_bench.rb
@@ -1,10 +1,2 @@
 require 'rails_bench/engine'
 require 'rails_bench/config'
-
-module Bench
-
-  def self.use_relative_model_naming?
-    true
-  end
-
-end
