4ebf47c672fb096d582224a235c097279f9a9cf2
qinmingyuan <mingyuan0715@foxmail.com>
2022-03-27 15:57:19 +0800

    task

--- app/models/bench/task.rb
+++ app/models/bench/task.rb
@@ -2,6 +2,6 @@ module Bench
   class Task < ApplicationRecord
     include Model::Task
     include Com::Ext::Taxon
-    include Eventual::Model::Planned if defined? RailsEvent
+    include Eventual::Ext::Planned if defined? RailsEvent
   end
 end
