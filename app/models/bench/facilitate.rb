ca239307c671e4d4df1cc31ef5b93c6ff7850895
qinmingyuan <mingyuan0715@foxmail.com>
2022-06-16 18:16:51 +0800

    docker update

--- app/models/bench/facilitate.rb
+++ app/models/bench/facilitate.rb
@@ -1,7 +1,7 @@
 module Bench
   class Facilitate < ApplicationRecord
     include Model::Facilitate
-    include Trade::Model::Good if defined? RailsTrade
+    include Trade::Ext::Good if defined? RailsTrade
     include Factory::Model::Good if defined? RailsFactory
     include Detail::Ext::Listing if defined? RailsDetail
   end
