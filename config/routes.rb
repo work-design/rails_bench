2b9c12bc6e0b3b1a37c4d1a1dc9c2a03bf8545d2
qinmingyuan <mingyuan0715@foxmail.com>
2022-07-24 20:43:31 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,145 +1,146 @@
 Rails.application.routes.draw do
-  concern :tasked do
-    collection do
-      get 'template' => :new_template
-    end
-    member do
-      patch :close
-      patch :reorder
-      patch :child
-      get :project
-      get 'done' => :edit_done
-      patch 'done' => :update_done
-      patch :rework
-      get 'focus' => :edit_focus
-      get 'assign' => :edit_assign
-      get :estimated
-      get :stock
-      get :fund
-      get :member
-      get :deadline
-    end
-    resources :task_timers do
-      member do
-        patch :pause
-      end
-    end
-    resources :pictures
-    resources :links
-  end
-
-  namespace 'bench', defaults: { business: 'bench' } do
-    resources :projects do
-      member do
-        match :github, via: [:get, :post]
-      end
-      resources :fund_incomes
-    end
-    resources :facilitates, only: [:index, :show] do
+  scope RailsExtend.default_routes_scope do
+    concern :tasked do
       collection do
-        get :buy
+        get 'template' => :new_template
       end
+      member do
+        patch :close
+        patch :reorder
+        patch :child
+        get :project
+        get 'done' => :edit_done
+        patch 'done' => :update_done
+        patch :rework
+        get 'focus' => :edit_focus
+        get 'assign' => :edit_assign
+        get :estimated
+        get :stock
+        get :fund
+        get :member
+        get :deadline
+      end
+      resources :task_timers do
+        member do
+          patch :pause
+        end
+      end
+      resources :pictures
+      resources :links
     end
 
-    namespace :me, defaults: { namespace: 'me' } do
-      resources :tasks do
-        concerns :tasked
-      end
+    namespace 'bench', defaults: { business: 'bench' } do
       resources :projects do
         member do
-          get :repos
-          get :github_hook
+          match :github, via: [:get, :post]
         end
-        resources :expenses
+        resources :fund_incomes
       end
-      resources :facilitates, only: [] do
-        member do
-          put :order
-        end
-      end
-    end
-
-    namespace 'admin', defaults: { namespace: 'admin' } do
-      resources :project_stages
-      resources :project_states
-      resources :projects do
-        member do
-          get :sync
-        end
-        resources :budgets
-        resources :expenses
-        resources :project_facilitates do
-          collection do
-            get :facilitates
-            get :providers
-          end
-        end
-        resources :project_indicators
-        resources :project_mileposts, except: [:index, :show]
-      end
-      resources :tasks do
+      resources :facilitates, only: [:index, :show] do
         collection do
-          get 'project/:project_id' => :project
-        end
-        concerns :tasked
-      end
-      scope path: ':financial_type/:financial_id' do
-        resources :fund_budgets
-        resources :fund_expenses
-      end
-      resources :mileposts do
-        member do
-          patch :move_lower
-          patch :move_higher
+          get :buy
         end
       end
-    end
 
-    namespace :panel, defaults: { namespace: 'panel' } do
-      resources :taxons do
-        member do
-          get :budgets
-          get :expenses
-          get :parameter
+      namespace :me, defaults: { namespace: 'me' } do
+        resources :tasks do
+          concerns :tasked
         end
-        resources :task_templates do
-          collection do
-            get :add
-            get :departments
-            get :job_titles
-            get :members
-          end
+        resources :projects do
           member do
-            get 'member' => :edit_member
-            patch 'member' => :update_member
-            patch :reorder
+            get :repos
+            get :github_hook
+          end
+          resources :expenses
+        end
+        resources :facilitates, only: [] do
+          member do
+            put :order
           end
         end
-        resources :taxon_facilitates do
-          collection do
-            get :facilitates
-          end
-        end
-        resources :taxon_indicators do
-          collection do
-            get :indicators
-          end
-        end
-        resources :projects
       end
-      resources :indicators
-      resources :facilitate_taxons
-      resources :facilitates do
-        resources :facilitate_providers
-        resources :facilitate_indicators
-      end
-    end
 
-    namespace :org, defaults: { namespace: 'org' } do
-      resources :facilitates do
-        resources :facilitate_providers
+      namespace 'admin', defaults: { namespace: 'admin' } do
+        resources :project_stages
+        resources :project_states
+        resources :projects do
+          member do
+            get :sync
+          end
+          resources :budgets
+          resources :expenses
+          resources :project_facilitates do
+            collection do
+              get :facilitates
+              get :providers
+            end
+          end
+          resources :project_indicators
+          resources :project_mileposts, except: [:index, :show]
+        end
+        resources :tasks do
+          collection do
+            get 'project/:project_id' => :project
+          end
+          concerns :tasked
+        end
+        scope path: ':financial_type/:financial_id' do
+          resources :fund_budgets
+          resources :fund_expenses
+        end
+        resources :mileposts do
+          member do
+            patch :move_lower
+            patch :move_higher
+          end
+        end
+      end
+
+      namespace :panel, defaults: { namespace: 'panel' } do
+        resources :taxons do
+          member do
+            get :budgets
+            get :expenses
+            get :parameter
+          end
+          resources :task_templates do
+            collection do
+              get :add
+              get :departments
+              get :job_titles
+              get :members
+            end
+            member do
+              get 'member' => :edit_member
+              patch 'member' => :update_member
+              patch :reorder
+            end
+          end
+          resources :taxon_facilitates do
+            collection do
+              get :facilitates
+            end
+          end
+          resources :taxon_indicators do
+            collection do
+              get :indicators
+            end
+          end
+          resources :projects
+        end
+        resources :indicators
+        resources :facilitate_taxons
+        resources :facilitates do
+          resources :facilitate_providers
+          resources :facilitate_indicators
+        end
+      end
+
+      namespace :org, defaults: { namespace: 'org' } do
+        resources :facilitates do
+          resources :facilitate_providers
+        end
       end
     end
   end
-
 end
