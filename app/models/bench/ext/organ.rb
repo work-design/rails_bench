6d8545fe176edae5882a48e9ced615047d285538
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-06 15:39:13 +0800

    bench cilitate

---
+++ app/models/bench/ext/organ.rb
@@ -0,0 +1,10 @@
+module Bench
+  module Ext::Organ
+    extend ActiveSupport::Concern
+
+    included do
+      has_many :facilitate_providers, class_name: 'Bench::FacilitateProvider', foreign_key: :provider_id, dependent: :destroy
+    end
+
+  end
+end
