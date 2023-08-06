c6e06472ba18f650703da3d4b7c9d368afdd95b1
qinmingyuan <mingyuan0715@foxmail.com>
2023-08-06 15:42:21 +0800

    gem

--- app/models/bench/ext/organ.rb
+++ app/models/bench/ext/organ.rb
@@ -3,7 +3,6 @@ module Bench
     extend ActiveSupport::Concern
 
     included do
-      has_many :facilitate_providers, class_name: 'Bench::FacilitateProvider', foreign_key: :provider_id, dependent: :destroy
     end
 
   end
