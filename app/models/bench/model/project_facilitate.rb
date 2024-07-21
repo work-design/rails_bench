e7b4c5185b710707b6387280658bf21939c4b951
qinmingyuan <mingyuan0715@foxmail.com>
2024-07-21 17:22:03 +0800

    project faciliate

--- app/models/bench/model/project_facilitate.rb
+++ app/models/bench/model/project_facilitate.rb
@@ -6,7 +6,8 @@ module Bench
       belongs_to :project
       belongs_to :facilitate_taxon
       belongs_to :facilitate
-      belongs_to :provider, class_name: 'Organ', optional: true
+
+      belongs_to :provider, class_name: 'Org::Organ', optional: true
       belongs_to :facilitate_provider, ->(o) { where(facilitate_id: o.facilitate_id) }, foreign_key: :provider_id, primary_key: :provider_id, optional: true
 
       before_validation :sync_facilitate_taxon, if: -> { facilitate_id_changed?}
